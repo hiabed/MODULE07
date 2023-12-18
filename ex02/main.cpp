b3c979a0d89839a37c33575ad50c60ea110c9ae4
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2023-12-18 17:58:27 +0100

    ex02 in progress

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-    int * a = new int();
-    std::cout << *a << std::endl;
+    int * a = new int[3]();
+    std::cout << a[0] << std::endl;
+    std::cout << a[1] << std::endl;
+    std::cout << a[2] << std::endl;
+    std::cout << a[3] << std::endl;
 }
