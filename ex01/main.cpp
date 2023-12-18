4237c275f390a7044d9081728a9841400c667517
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2023-12-18 14:08:01 +0100

    ex01 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,18 +3,18 @@
 int main()
 {
     std::cout << "int array\n";
-    int arr[] = {1, 2, 3, 4};
-    iter(arr, 4, print);
+    const int arr[] = {1, 2, 3, 4};
+    iter(arr, 4, &print);
 
     std::cout << "\narray of characters\n";
-    char s[] = "hello";
-    iter(s, 5, print);
+    const char s[] = "hello";
+    iter(s, 5, &print);
 
     std::cout << "\ndouble array\n";
-    double dArr[] = {1.2, 2.3, 3.4, 4.5};
-    iter(dArr, 4, print);
+    const double dArr[] = {1.2, 2.3, 3.4, 4.5};
+    iter(dArr, 4, &print);
 
     std::cout << "\narray of strings\n";
-    std::string strings[] = {"hello", "world", "!!"};
-    iter(strings, 3, print);
+    const std::string strings[] = {"hello", "world", "!!"};
+    iter(strings, 3, &print);
 }
